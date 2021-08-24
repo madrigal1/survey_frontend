88a49414f09b8a900010c25702a4c5c8fa96d518
madrigal1 <kurupgokul11@gmail.com>
2021-08-24 10:52:56 +0530

    feat: added scatter graph

--- src/app/survey-analytics/survey-analytics.component.ts
+++ src/app/survey-analytics/survey-analytics.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ChartType } from 'chart.js';
-import { SingleDataSet } from 'ng2-charts';
+import { ChartOptions, ChartType } from 'chart.js';
+import { Label, SingleDataSet } from 'ng2-charts';
 import { SurveyComponentType } from '../survey-generator/survey-generator.component';
 import { SurveyService } from '../survey.service';
 @Component({
@@ -25,6 +25,7 @@ export class SurveyAnalyticsComponent implements OnInit {
   };
   public pieChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   public pieChartType: ChartType = 'pie';
+  public scatterChartType: ChartType = 'scatter';
   public pieChartLegend = true;
   public barChartData = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
@@ -35,6 +36,16 @@ export class SurveyAnalyticsComponent implements OnInit {
       backgroundColor: ['#4F549E', '#D65DB1', '#032B44', '#59293E', '#A64C73'],
     },
   ];
+  public scatterChartOptions: any = {
+    responsive: true,
+    scales: {
+      x: {
+        type: 'linear',
+        position: 'bottom'
+      }
+    },
+  };
+  public scatterChartLabels: Label[] = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'];
   constructor(private route: ActivatedRoute, private surveyService: SurveyService) {
     this.selectionAns = [];
     this.numericalAns = [];
@@ -80,10 +91,10 @@ export class SurveyAnalyticsComponent implements OnInit {
         })
   }
 
-  getSelectionData(question_id: string, labels: boolean): any {
+  getSelectionData(arr: Array<any>, question_id: string, labels: boolean): any {
     //console.log("input", this.selectionAns, question_id);
     //console.log("qid", question_id);
-    let filteredAns = this.selectionAns.filter((ele: any) => ele.question_id === question_id)
+    let filteredAns = arr.filter((ele: any) => ele.question_id === question_id)
     // console.log("filter", filteredAns);
     if (filteredAns.length == 0)
       return [];
@@ -94,6 +105,26 @@ export class SurveyAnalyticsComponent implements OnInit {
     //   data: Object.values(wf),
     //   label: "No of answers with that option"
     // }]
+  }
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  getNumericalData(question_id: string) {
+    let res: any = [];
+    let filteredAns = this.numericalAns.filter((ele: any) => ele.question_id === question_id);
+    filteredAns.forEach(({ main_answer }: any, index: number) => {
+      res.push({
+        x: index + 1,
+        y: main_answer,
+      });
+    })
+    console.log("num", res);
+    return res;
   }
   onSelect(event: any) {
     console.log(event);
